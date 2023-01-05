9618e397d657ac189ee5e6f09ac48725d55c0dc9
kqby <Qb3tty@gmail.com>
2023-01-05 23:42:21 +0100

    componensek alakitása

---
+++ src/app/hotels/hotels.model.js.ts
@@ -0,0 +1,8 @@
+export interface Hotel{
+  id:number;
+  name:string;
+  address:string;
+  picture:string;
+  description:string
+
+}
---
+++ src/app/hotels/hotels.storage.ts
@@ -0,0 +1,44 @@
+import {Hotel} from "./hotels.model.js";
+
+export const Hotels: Array<Hotel> = [
+  {
+    id:   1,
+    name: 'Para Panzio',
+    address: 'Kossuth Lsgt 71',
+    picture:'honey01.jpg',
+    description:'Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandaeLorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. EosEosEos inventore ab autem cupiditate est laborum iusto. Eos'
+
+  },
+  {
+    id: 2,
+    name: 'Para Panziosdds',
+    address: 'Kossuth Lsgt 71',
+    picture:'honey01.jpg',
+    description:'Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandaeLorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. EosEosEos inventore ab autem cupiditate est laborum iusto. Eos'
+
+  },
+  {
+    id:   3,
+    name: 'Para Panzio12',
+    address: 'Kossuth Lsgt 71',
+    picture:'honey01.jpg',
+    description:'Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandaeLorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. EosEosEos inventore ab autem cupiditate est laborum iusto. Eos'
+
+  },
+  {
+    id: 4,
+    name: 'Para Panzio12',
+    address: 'Kossuth Lsgt 71',
+    picture:'honey01.jpg',
+    description:'Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandaeLorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. EosEosEos inventore ab autem cupiditate est laborum iusto. Eos'
+
+  },
+  {
+    id: 5,
+    name: 'Para Panzio22',
+    address: 'Kossuth Lsgt 71',
+    picture:'honey01.jpg',
+    description:'Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandaeLorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. Lorem ipsum dolor sit amet. Eos tempore quisquam nam dolorum quas est modi illum id repudiandae inventore ab autem cupiditate est laborum iusto. EosEosEos inventore ab autem cupiditate est laborum iusto. Eos'
+
+  },
+];
